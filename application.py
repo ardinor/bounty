8870f42e81c40a47419906bfc9750524a31460b9
ardinor <temnoyume@gmail.com>
2013-03-12 16:44:59 +1000

    Some more work, still very basic, some templates and db usage

--- application.py
+++ application.py
@@ -2,32 +2,52 @@
 
 import tornado.ioloop
 import tornado.web
+import tornado.options
+import tornado.httpserver
+
+import pymongo
+import os
 
 from admin import AdminHandler
 from fundraiser import FundraiserCreateHandler
 from fundraiser import FundraiserEditHandler
 
 
+class Application(tornado.web.Application):
+
+    def __init__(self):
+
+        handlers = [
+                    (r'/', IndexHandler),
+                    (r'/login', LoginHandler),
+                    (r'/logout', LogoutHandler),
+                    (r'/admin', AdminHandler),
+                    (r'/fundraiser/create', FundraiserCreateHandler),
+                    (r'/fundraiser/([^/]+)/edit', FundraiserEditHandler),
+                 ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            debug=True,
+            )
+
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
 class BaseHandler(tornado.web.RequestHandler):
 
     @property
     def db(self):
-        return self.application.db
-
-    @property
-    def session(self):
-        return self.application.session
-
-    def get_current_user(self):
-        auth = self.get_secure_cookie('auth')
-        if not auth:
-            return None
+        conn = pymongo.Connection()
+        db = conn.bounty
+        return db
 
 
-class IndexHandler(tornado.web.RequestHandler):
+class IndexHandler(BaseHandler):
 
     def get(self):
-        self.render('index.html')
+        recent = self.db.fundraisers.find().sort('-launched').limit(15)
+        self.render('index.html', recent=recent)
 
 
 class LoginHandler(tornado.web.RequestHandler):
@@ -40,14 +60,7 @@ class LogoutHandler(tornado.web.RequestHandler):
 
 if __name__ == '__main__':
 
-    application = tornado.web.Application(
-        handlers=[
-                    (r'/', IndexHandler),
-                    (r'/login', LoginHandler),
-                    (r'/logout', LogoutHandler),
-                    (r'/admin', AdminHandler),
-                    (r'/fundraiser/create', FundraiserCreateHandler),
-                    (r'/fundraiser/([^/]+)/edit', FundraiserEditHandler),
-                 ], **{"template_path": "templates"})
-    application.listen(8899)
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
--- fundraiser.py
+++ fundraiser.py
@@ -3,7 +3,17 @@ import tornado.web
 
 
 class FundraiserCreateHandler(tornado.web.RequestHandler):
-    pass
+
+    def get(self):
+        self.render('fundraiser/create.html')
+
+    def post(self):
+        title = self.get_argument('title', None)
+        goal = self.get_argument('goal', None)
+        deadline = self.get_argument('deadline', None)
+        description = self.get_argument('description', None)
+
+        #sanitise
 
 
 class FundraiserEditHandler(tornado.web.RequestHandler):
