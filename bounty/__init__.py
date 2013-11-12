dc787ebf124d23125632dcbd14a24b943ad08b00
ardinor <temnoyume@gmail.com>
2013-11-12 11:51:11 +1000

    Some more basic work

--- bounty/__init__.py
+++ bounty/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 from settings import DEBUG, SECRET_KEY
+from moment_js import moment_js
 
 app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
+
+app.jinja_env.globals['moment_js'] = moment_js
